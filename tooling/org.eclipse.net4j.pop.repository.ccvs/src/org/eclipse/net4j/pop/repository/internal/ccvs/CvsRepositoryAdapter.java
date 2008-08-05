2f8c91e7dd34772f4fcf90199515fca88d9e6fc1
Eike Stepper <stepper@esc-net.de>
2008-08-05 10:45:20 +0000

    [241121]  Develop POP Process Tooling https://bugs.eclipse.org/bugs/show_bug.cgi?id=241121

---
+++ tooling/org.eclipse.net4j.pop.repository.ccvs/src/org/eclipse/net4j/pop/repository/internal/ccvs/CvsRepositoryAdapter.java
@@ -0,0 +1,58 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2008 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.net4j.pop.repository.internal.ccvs;
+
+import org.eclipse.net4j.pop.internal.repository.RepositoryAdapter;
+import org.eclipse.net4j.pop.repository.ccvs.ICvsRepositoryAdapter;
+import org.eclipse.net4j.util.ui.UIUtil;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.team.internal.ccvs.core.ICVSRemoteFolder;
+import org.eclipse.team.internal.ccvs.ui.operations.CheckoutIntoOperation;
+import org.eclipse.team.ui.TeamOperation;
+import org.eclipse.ui.IWorkbenchPart;
+
+import java.util.Date;
+
+/**
+ * @author Eike Stepper
+ */
+public class CvsRepositoryAdapter extends RepositoryAdapter implements ICvsRepositoryAdapter
+{
+  public CvsRepositoryAdapter()
+  {
+  }
+
+  @Override
+  public void checkoutBranch(String branchName, IPath location)
+  {
+    ICVSRemoteFolder remoteFolder = null;
+    IContainer localFolder = null;
+    TeamOperation operation = new CheckoutIntoOperation(getPart(), remoteFolder, localFolder, true);
+    operation.run();
+  }
+
+  @Override
+  public void checkoutDate(Date date, IPath location)
+  {
+  }
+
+  @Override
+  public void checkoutTag(String tagName, IPath location)
+  {
+  }
+
+  private static IWorkbenchPart getPart()
+  {
+    return UIUtil.getActiveWorkbenchPart();
+  }
+}
---
+++ tooling/org.eclipse.net4j.pop.repository/src/org/eclipse/net4j/pop/repository/IRepositoryAdapter.java
@@ -0,0 +1,28 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2008 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.net4j.pop.repository;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+
+import java.util.Date;
+
+/**
+ * @author Eike Stepper
+ */
+public interface IRepositoryAdapter extends IAdaptable
+{
+  public void checkoutBranch(String branchName, IPath location);
+
+  public void checkoutDate(Date date, IPath location);
+
+  public void checkoutTag(String tagName, IPath location);
+}
